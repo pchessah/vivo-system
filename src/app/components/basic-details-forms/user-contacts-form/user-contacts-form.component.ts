0758ffedf50359efae78062f6ace4366b62756eb
Paul Chesa <paulchesa1@gmail.com>
2020-12-31 13:01:22 +0300

    Design of user contact info form

--- src/app/components/basic-details-forms/user-contacts-form/user-contacts-form.component.ts
+++ src/app/components/basic-details-forms/user-contacts-form/user-contacts-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-contacts-form',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-contacts-form.component.scss']
 })
 export class UserContactsFormComponent implements OnInit {
+  userContactForm: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userContactForm = this.fb.group({
+      email: ['', Validators.required],
+      firstName: ["",Validators.required],
+      lastName: ["",Validators.required],
+      otherNames: [""],
+      phoneNumber: ["",Validators.required],
+      address: ["",Validators.required],
+      postalCode: ["",Validators.required],
+    })
+  }
+
+  saveContactInfo(){
+    console.log(this.userContactForm.value);
   }
 
 }
