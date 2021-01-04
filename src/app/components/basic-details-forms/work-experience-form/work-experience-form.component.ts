ccaebdcca1910ad677a017708d5eb7c8af5be2ff
Paul Chesa <paulchesa1@gmail.com>
2021-01-04 11:17:58 +0300

    work experience form designed

--- src/app/components/basic-details-forms/work-experience-form/work-experience-form.component.ts
+++ src/app/components/basic-details-forms/work-experience-form/work-experience-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { LocalStorageService } from 'src/app/libs/services/local-storage/local-storage.service';
+import { UserFeedbackMessagesService } from 'src/app/libs/services/user-feedback-messages/user-feedback-messages.service';
+
 
 @Component({
   selector: 'app-work-experience-form',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-experience-form.component.scss']
 })
 export class WorkExperienceFormComponent implements OnInit {
+  @Output() nextSectionEvent = new EventEmitter<string>();
+  workExperienceForm: any
 
-  constructor() { }
+  constructor( private fb: FormBuilder, private LocalStorageService: LocalStorageService, private userFeedBackMessageService: UserFeedbackMessagesService) { }
 
   ngOnInit(): void {
+    this.workExperienceForm = this.fb.group({
+      position: [null,[Validators.required, Validators.email]],
+      startDate: [null, Validators.required],
+      endDate: [null, Validators.required],
+      company: [null, Validators.required]
+    })
   }
 
 }
